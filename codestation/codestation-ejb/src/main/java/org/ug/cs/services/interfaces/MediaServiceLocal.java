6b21c0c9d1079cd36ba6450ed19e38c792b3bc50
KL <karim.lahouague@esprit.tn>
2015-02-15 01:29:37 +0100

    Signed-off-by: KL <karim.lahouague@esprit.tn>

--- codestation/codestation-ejb/src/main/java/org/ug/cs/services/interfaces/MediaServiceLocal.java
+++ codestation/codestation-ejb/src/main/java/org/ug/cs/services/interfaces/MediaServiceLocal.java
@@ -1,6 +1,19 @@
 package org.ug.cs.services.interfaces;
 
+import java.util.List;
+
+import org.ug.cs.persistence.Media;
 
 public interface MediaServiceLocal {
+
+	void create(Media media);
+
+	void update(Media media);
+
+	Media findById(Integer id);
+
+	List<Media> findAll();
+
+	Long count();
 
 }
--- codestation/codestation-ejb/src/main/java/org/ug/cs/services/interfaces/MediaServiceRemote.java
+++ codestation/codestation-ejb/src/main/java/org/ug/cs/services/interfaces/MediaServiceRemote.java
@@ -1,6 +1,21 @@
 package org.ug.cs.services.interfaces;
 
+import java.util.List;
+
+import org.ug.cs.persistence.Media;
+
 
 public interface MediaServiceRemote {
+	
+	void create(Media media);
+	
+	void update(Media media);
+	
+	Media findById(Integer id);
+	
+	List<Media> findAll();
+	
+	Long count();
+	
 
 }
